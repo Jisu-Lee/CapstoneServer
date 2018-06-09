dfd8255a071195e463183da06d40ad23a2c68d3d
Jisu-Lee <leejisu0202@gmail.com>
2018-06-09 19:43:29 +0900

    final commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cgi
 import datetime
 import webapp2
 import datetime
+from operator import itemgetter
 #import numpy as np
 #import matplotlib.pyplot as plt
 #import tensorflow
@@ -19,7 +20,7 @@ class FineDust(ndb.Model):
   value = ndb.FloatProperty()
   humidity = ndb.FloatProperty()
   temperature = ndb.FloatProperty()
-  date = ndb.DateTimeProperty()
+  date = ndb.DateTimeProperty(indexed=True)
   location = ndb.StringProperty(indexed=True)
 # [END create_db]
 
@@ -51,20 +52,79 @@ def getdata() :
         return "no json recieved\n"
 
 # [START graph]
+@app.route('/graph')
+def graph():
+    r_entities = ndb.gql('SELECT * '
+                       'FROM FineDust '
+                       'ORDER BY date ASC LIMIT 100'
+                       ,)
+    #from operator import itemgetter
+    #newlist = sorted(list_to_be_sorted, key=itemgetter('name')) 
+    entities = sorted(r_entities, key=itemgetter('date'))
 
+    # create response
+    response = """<html>
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
 
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('datetime', 'date');
+      data.addColumn('number', 'humidity');
+      data.addColumn('number', 'temperature');
+      data.addColumn('number', 'concentration');
+
+      data.addRows([
+        """
+    #myDate = datetime.strptime(str(entity.date),'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+    i = 1
+    First = True
+    for entity in entities:
+        response += '[ new Date(' + str(entity.date.year) + ',' + str(entity.date.month) + ',' + str(entity.date.day) + ',' + str(entity.date.hour) + ',' + str(entity.date.minute) + ',' + str(entity.date.second) +'),'
+        response += str(entity.humidity) + ','
+        response += str(entity.temperature) + ','
+        response += str(entity.value) + '],'
+        
+    response += '[new Date(2018, 6, 3, 03, 14, 30), 2, 3, 4]'
+
+    response += """
+      ]);
+
+      var options = {
+        chart: {
+          title: '100 data collected most recently',
+          subtitle: 'humidity, concentration, temperature'
+        },
+        width: 900,
+        height: 500,
+        axes: {
+          x: {
+            0: {side: 'top'}
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+  </script>
+</head>
+<body>
+  <div id="line_top_x"></div>
+</body>
+</html>"""
+
+    return response
 # [END graph]
 
 # [START expectation]
 @app.route('/expectation')
 def expectation() :
-    '''
-    app.logger.debug("data received...")
-    app.logger.debug(request.json)
-
-    data = request.json
-    futuredate = data['date']
-'''
     # fetch database
     entities = ndb.gql('SELECT date, value '
                        'FROM FineDust '
@@ -74,11 +134,11 @@ def expectation() :
     for entity in entities:
         x.append(entity.date)
         y.append(entity.value)
-    response = 'Average concentration at HyungJune\'s room : ' + str(sum(y)/len(y)) + '<br>'
+    response = 'Average concentration at HyeongJune\'s room : ' + str(sum(y)/len(y)) + '<br>'
 
     entities = ndb.gql('SELECT date, value '
                'FROM FineDust '
-               'WHERE location = \'6P\'',)
+               'WHERE location = \'5P\'',)
     x = []
     y = []
     for entity in entities:
@@ -88,7 +148,73 @@ def expectation() :
     logging.info(x)
     logging.info(y)
    
-    response = response + 'Average concentration at 6P : ' + str(sum(y)/len(y)) + '\n'
+    response = response + 'Average concentration at 5P : ' + str(sum(y)/len(y)) + '<br>'
+
+    entities = ndb.gql('SELECT * '
+                       'FROM FineDust '
+                       'ORDER BY date ASC LIMIT 100'
+                       ,)
+    #from operator import itemgetter
+    #newlist = sorted(list_to_be_sorted, key=itemgetter('name')) 
+    # entities = sorted(r_entities, key=itemgetter('date'))
+
+    # create response
+    response += """<html>
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('datetime', 'date');
+      data.addColumn('number', 'humidity');
+      data.addColumn('number', 'temperature');
+      data.addColumn('number', 'concentration');
+
+      data.addRows([
+        """
+    #myDate = datetime.strptime(str(entity.date),'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+    i = 1
+    First = True
+    for entity in entities:
+        response += '[ new Date(' + str(entity.date.year) + ',' + str(entity.date.month) + ',' + str(entity.date.day) + ',' + str(entity.date.hour) + ',' + str(entity.date.minute) + ',' + str(entity.date.second) +'),'
+        response += str(entity.humidity) + ','
+        response += str(entity.temperature) + ','
+        response += str(entity.value) + '],'
+        
+    response += '[new Date(2018, 6, 3, 03, 14, 30), 2, 3, 4]'
+
+    response += """
+      ]);
+
+      var options = {
+        chart: {
+          title: '100 data collected most recently',
+          subtitle: 'humidity, concentration, temperature'
+        },
+        width: 900,
+        height: 500,
+        axes: {
+          x: {
+            0: {side: 'top'}
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+  </script>
+</head>
+<body>
+  <div id="line_top_x"></div>
+</body>
+</html>"""
+
     return response
 # [END expectation]
 
