0ff4b23c8c01c7839fe5e2ac10ff0972adfde4bd
Jisu-Lee <leejisu0202@gmail.com>
2018-06-10 17:24:51 +0900

    deleted form.html, submitted_form.html

--- main.py
+++ main.py
@@ -148,31 +148,6 @@ def expectation() :
     return response
 # [END expectation]
 
-# [START form]
-@app.route('/form')
-def form():
-    return render_template('form.html')
-# [END form]
-
-
-# [START submitted]
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
-    # [END submitted]
-    # [START render_template]
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
-    # [END render_template]
-
 
 @app.errorhandler(500)
 def server_error(e):
